cb9c12fa617f4c435d152638c1afbcc7f88169ed
MikkyMeow <arturakk2015@gmail.com>
2021-03-29 13:58:11 +0300

    added tags control panel

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Button, IconButton, Slider, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import {
+  Add,
+  AvTimer,
+  Pause,
+  PlayArrow,
+  Speed,
+  Stop,
+} from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,6 +17,11 @@ const useStyles = makeStyles((theme) => ({
     height: 812,
     border: '1px solid black',
   },
+  button: {
+    width: 150,
+    height: 60,
+    backgroundColor: 'red',
+  },
 }));
 
 export const Layout = () => {
@@ -16,7 +29,31 @@ export const Layout = () => {
 
   return (
     <Box className={classes.root}>
-      <h2>Hello</h2>
+      <Box>
+        <Typography>Текущий трек</Typography>
+        <Typography>Название трека</Typography>
+        <IconButton>
+          <Add />
+        </IconButton>
+        <Slider></Slider>
+        <Box>
+          <IconButton>
+            <AvTimer />
+          </IconButton>
+          <IconButton>
+            <PlayArrow />
+          </IconButton>
+          <IconButton>
+            <Pause />
+          </IconButton>
+          <IconButton>
+            <Stop />
+          </IconButton>
+          <IconButton>
+            <Speed />
+          </IconButton>
+        </Box>
+      </Box>
     </Box>
   );
 };
